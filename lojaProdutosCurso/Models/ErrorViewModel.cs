8ec04380bed9ed2153fd0819d66e0a65fa43ce96
Edivan <edivan220479@gmail.com>
2026-05-15 09:40:26 -0300

    commit inicial

---
+++ lojaProdutosCurso/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lojaProdutosCurso.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
