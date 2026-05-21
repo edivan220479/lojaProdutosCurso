f69214da266068cb13d9c11826c30b7ae3f911f8
Edivan <edivan220479@gmail.com>
2026-05-21 11:27:13 -0300

    Repository Pattern P2

--- lojaProdutosCurso/obj/Debug/net8.0/lojaProdutosCurso.AssemblyInfo.cs
+++ lojaProdutosCurso/obj/Debug/net8.0/lojaProdutosCurso.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lojaProdutosCurso")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a59016df0f207616a5f69e169a94d54d351e89e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b38ae89fa9a0701a0fa9006732c546f98d5178f")]
 [assembly: System.Reflection.AssemblyProductAttribute("lojaProdutosCurso")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lojaProdutosCurso")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
